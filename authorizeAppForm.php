72e5a5564fc5a36b911e27e0b21aeda1fbdea831
Joshua Adadevoh <tsc.joshua@gmail.com>
2014-12-16 19:04:06 -0500

    added drop box api functionality
    authenticate to dropbox api
    take pictures, and upload them to dropbox
    added semantic ui styling
    list and get meta data on files in dropbox folder

--- authorizeAppForm.php
+++ authorizeAppForm.php
@@ -1,11 +1,18 @@
-<div class="ui form">
-	<label><p>1. go <a href="<?php echo $authorizeUrl?>" target="_blank"> here</a> to authorize app</p></label>
-	<label><p>2. Click "Allow" (You might have to log in first) </p></label>
-	<form action="index.php" method="post">
-		<div class="inline field">
-			<label>Authorization Code</label>
-			<input type="text" name="authCode">
-			<input type="button" class="ui button">
+<div class="ui grid">
+	<div class="six wide column">
+		<div class="ui raised segment">
+			<div class="ui form">
+				<label><p>1. Click <a href="<?php echo $authorizeUrl?>" target="_blank"> here</a> to authorize app</p></label>
+				<label><p>2. Click "Allow" (You might have to log in first) </p></label>
+				<label><p>3. Copy the authorization code and paste it to the box below</p></label>
+				<form action="index.php" method="post">
+					<div class="inline field">
+						<label>Authorization Code</label>
+						<input type="text" name="authCode">
+						<input type="submit" class="ui button" name="submit">
+					</div>
+				</form>
+			</div>
 		</div>
-	</form>
+	</div>
 </div>
--- index.php
+++ index.php
@@ -7,15 +7,25 @@ $webAuth = new dbx\WebAuthNoRedirect($appInfo, "Mobiquity Challenge");
 $authorizeUrl = $webAuth->start();
 define("PICS_DIR", "/Pictures");
 
-//print_r($_SESSION);
+echo'
+	<div class="ui grid">
+		<div class="six wide column">
+			<div class="ui raised segment">
+				<div class="ui warning message">
+					<div class="header"> Please create folder "uploads" in project folder if not already done</div>
+				</div>
+			</div>
+		</div>
+	</div>';
 
-if(empty($_FILES['webcam'])){
-	echo"files webcam empty<br>";
+if(!empty($_POST['picture'])) {
+	$binary_data = base64_decode($_POST['picture']);
+	$result = file_put_contents('uploads/webcam.jpg', $binary_data);
+	if(!$result) die('Could not save image!  Check file permissions
+						or please create an "uploads" folder in your project<br>.');	
 }
 else{
-	print_r($_FILES['webcam']);
-	move_uploaded_file($_FILES['webcam']['temp'], 'webcam.jpg');
-	echo"<br>";
+	//add logic later
 }
 
 if(!empty($_POST['authCode'])) {
@@ -26,24 +36,18 @@ if(!empty($_POST['authCode'])) {
 	$_SESSION['accessToken'] = $accessToken;
 
 }
-
 if(!empty($_SESSION['accessToken'])){
 	$dbxClient = new dbx\Client($_SESSION['accessToken'], "Mob Challenge");
 	$accountInfo =$dbxClient->getAccountInfo();
-	print_r($accountInfo);
+	
 
-	$dbxClient->createFolder(PICS_DIR);
-	//file upload
-	$f = fopen("testFile.txt", "rb");
-	$result = $dbxClient->uploadFile(PICS_DIR."/testFile.txt", dbx\WriteMode::add(), $f);
-	fclose($f);
-
-	print_r($result);
-
-
-
+	$dbxClient->createFolder(PICS_DIR);/*echo $_POST['picUpload'];*/
+	if(!empty($_POST['picUpload']) ){
+		$f = fopen("uploads/webcam.jpg", "rb");
+		$result = $dbxClient->uploadFile(PICS_DIR."/image.jpg", dbx\WriteMode::add(), $f);
+		fclose($f);
+	}
 }
-
 
 ?>
 <!doctype html>
@@ -56,6 +60,25 @@ if(!empty($_SESSION['accessToken'])){
 		<script src="vendor/semantic/ui/dist/semantic.js" type="text/javascript"></script>
 		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 		<script type="text/javascript" src="webCam.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				Webcam.set({
+					width: 320,
+					height: 240,
+					image_format: 'jpeg',
+					jpeg_quality: 90
+				});
+				Webcam.attach('#camera');
+			});
+			function take_snapshot() {
+				Webcam.snap( function(data_uri) {
+					var raw_image_data = data_uri.replace(/^data\:image\/\w+\;base64\,/, '');
+					$('input[name=picture]').val(raw_image_data);
+					$('form').submit();
+
+				});
+			}
+	</script>
 	</head>
 	<body>
 		<?php
@@ -63,8 +86,40 @@ if(!empty($_SESSION['accessToken'])){
 			include"authorizeAppForm.php";
 		}
 		else{
-			include"appForm.php";
+			?>
+			<div class="ui grid">
+				<div class="six wide column">
+					<div class="ui raised segment">
+						<div id="camera" ></div>
+						<form method="post" action="index.php">
+		    				<input id="mydata" type="hidden" name="picture" value=""/>
+		    				<input type="button" class="green ui button" value="Take Snapshot" onClick="take_snapshot()" />
+						</form>
+					</div>
+
+				</div>
+				<div class="six wide column">
+					<div id="preview" class="ui raised segment"><?php 
+						$d = rand();
+						echo'<img id="prev"  src="uploads/webcam.jpg?'.$d.'"/>';
+						?>				
+						<form method="post" action="index.php">
+							<input id="picUpload" type="hidden" name="picUpload" value="pizzur" />
+							<input type="submit" class="blue ui button"  value="upload to DropBox" />
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<div class="ui raised segment">
+				<p>List of items in folder. Sorry, I couldnt figure out how to proper display them in time for submission</p>
+				<?php
+				$folderMetadata = $dbxClient->getMetadataWithChildren("/");
+				print_r($folderMetadata['contents']);
+				?>
+			</div>
+			<?php
 		}
-		?>
+			?>
 	</body>
 </html>
